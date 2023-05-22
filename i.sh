1ba3ec11e0c618a7ec726c33fe94328f4bcd0ead
Jamie Shield <2021765+jamieshield@users.noreply.github.com>
2023-05-22 10:58:06 +1000

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-
-# Web console access?
-echo -e "${PASSWD}\n${PASSWD}" | sudo passwd opc
-echo -e "${PASSWD}\n${PASSWD}" | sudo passwd ubuntu
-
+# curl https://raw.githubusercontent.com/jamieshield/coit11241/main/i.sh | sudo PASSWD="pass" bash -s -
 if [ ! -e /mnt/1GiB.swap ] ; then
+	# Web console access? Not for terminal login
+	echo -e "${PASSWD}\n${PASSWD}" | sudo passwd opc
+	echo -e "${PASSWD}\n${PASSWD}" | sudo passwd ubuntu
+
 	# https://help.ubuntu.com/community/SwapFaq
 	sudo swapoff -a	
 	sudo dd if=/dev/zero of=/mnt/1GiB.swap bs=1024 count=1048576
@@ -17,8 +17,9 @@ fi
 if [ ! -e /etc/ajenti ] ; then
   # https://docs.ajenti.org/en/latest/man/install.html
   curl https://raw.githubusercontent.com/ajenti/ajenti/master/scripts/install.sh | sudo bash -s -
+  #sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 8000 -j ACCEPT
+  #sudo netfilter-persistent save
   sudo firewall-cmd --permanent --zone=public --add-port=8000/tcp
   sudo firewall-cmd --zone=public --add-port=8000/tcp
-  ROOTPASSWD="vagrant"
-  echo -e "${ROOTPASSWD}\n${ROOTPASSWD}" | sudo passwd root
+  echo -e "${PASSWD}\n${PASSWD}" | sudo passwd root
 fi
