2617c9427d361acfdbc4d27b39032222617cfe79
Jamie Shield <2021765+jamieshield@users.noreply.github.com>
2023-05-24 13:35:39 +1000

    Update configWazuh.py

--- configWazuh.py
+++ configWazuh.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from xml.dom import minidom
 
 # agent.conf needed too
@@ -12,9 +12,9 @@ ossecConf=minidom.parseString(xml)
 vd=ossecConf.getElementsByTagName("vulnerability-detector")[0]
 en=vd.getElementsByTagName("enabled")[0]
 en.firstChild.nodeValue="yes"
-vd=ossecConf.getElementsByTagName("vulnerability-detector")[0]
-en=vd.getElementsByTagName("enabled")[0]
-print(en.firstChild.nodeValue)
+vd=ossecConf.getElementsByTagName("remote")[0]
+en=vd.getElementsByTagName("protocol")[0]
+en.firstChild.nodeValue="udp"
 f=open("/var/ossec/etc/ossec.conf",'w')
 # skip <a/>
 for n in ossecConf.firstChild.childNodes:
