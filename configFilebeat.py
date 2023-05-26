da54fe8a20b237b18c61136028603ce01f72b46c
Jamie Shield <2021765+jamieshield@users.noreply.github.com>
2023-05-26 10:17:27 +1000

    Update configFilebeat.py

--- configFilebeat.py
+++ configFilebeat.py
@@ -1,9 +1,13 @@
 
 # Usage: curl https://raw.githubusercontent.com/jamieshield/coit11241/main/configFilebeat.py | sudo python -
 # Disable filebeat alerts.
-import yaml
+import sys
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
 with open("/etc/filebeat/filebeat.yml.orig") as f:
-    doc=yaml.safe_load(f)
+    doc=yaml.load(f)
 
 #filebeat.modules:
 #  - module: wazuh
